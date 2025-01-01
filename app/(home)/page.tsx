cb068b8324da7a11f11cd2438a435c9ec83f975e
SongJunHyuk <83599564+FE-Nerdy@users.noreply.github.com>
2025-01-01 23:18:20 +0900

    Data fetching

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,21 @@
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+import Link from "next/link";
+
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch(URL);
-  const json = await response.json();
-  return json;
+    ;
+    const response = await fetch(API_URL);
+    const json = await response.json();
+    return json;
 }
 export default async function HomePage() {
-  const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+    const movies = await getMovies();
+    return (
+        <div>
+            {movies.map((movie) => (
+                <li key={movie.id}>
+                    <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+                </li>
+            ))}
+        </div>
+    );
 }
